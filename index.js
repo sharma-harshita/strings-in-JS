b3041f421721e00b9ed623aec1b84fcfe2b703d6
harshita.sharma <harshita.sharma@asksid.ai>
2021-12-30 20:33:05 +0530

    first

---
+++ index.js
@@ -0,0 +1,54 @@
+console.log("Hello world !!");
+
+
+let course = "Full Stack Web Development Course";
+
+
+// How to find the length of the string. [len]
+// const len = course.length;
+// console.log("Length of the string is :", len);
+
+
+
+// Concatination of the strings    [+, concat]
+// let name1 = "Vijay Pratap Singh";
+// let age = "22";
+// let statement = name1 + " is " + age + " years old."
+// let newStr = age.concat(name1);
+// console.log(newStr);
+// console.log(name1 + age);
+// console.log(statement);
+
+
+// Access any character based on what ? index value.
+// var a = [1, 3, 5]
+// console.log(course[10]);        // it will return you the character present at that specific index
+// console.log(course.indexOf("l"));   // this will return the index of that character at first occurence.
+// console.log(course.lastIndexOf("l")) // this will return the index of the character of last occurence
+
+
+
+// ****************** IMPORTANT ***************
+// REVERSE OF A STRING : 
+
+
+// 1.> split : which converts your string to an array.
+
+let name = "Mayank";
+let aplhabet = "a b c"
+const splitStr = name.split("")
+const splitStr2 = aplhabet.split(" ")
+
+console.log(splitStr, splitStr2);
+
+
+// 2.> reverse : reverse all the values present inside it
+
+const reversedArr = splitStr.reverse();
+console.log("Reversed Array",reversedArr);
+
+
+// 3.>  join   : convert the array to string
+
+const strOutput = reversedArr.join("*");
+console.log("Output", strOutput);
